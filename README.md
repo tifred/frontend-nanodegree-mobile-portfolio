eb9f6e3319948fd396553bcd2ba768ec20e30628
System Administrator <root@Ti-Freds-MacBook-Pro.local>
2015-11-04 12:38:44 -0800

    final changes

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ TO RUN APPLICATION:
 1. Load dist/index.html in browser.
 check speed in: https://developers.google.com/speed/pagespeed/insights/
 
-optional: url to project on github pages (github web server uses gzip compression):  
+note: performance improves when web server does automatic gzipping.
+the same page, published on github pages will do this:
 http://tifred.github.io/frontend-nanodegree-mobile-portfolio/dist/index.html
 
 2. Load dist/views/pizza.html
-Scroll
+Scroll.
 Frame rate should be 60 fps.
 
 3. Load dist/views/pizza.html
@@ -24,26 +25,32 @@ Reduced Critical Rendering Path by:
 --inlining style.css file into head of index.html.
 --adding media condition to css relevant to printing only.
 --puting print only css in separate file that is loaded as needed.
---cropping and compressing image "profilepic.jpg".
---cropping and compressing image "views/images/pizzeria.jpg"
---moving JS from js/perfmatters.js into index.html, with async tag
+--compressing image "profilepic.jpg".
+--cropping and compressing image "views/images/pizzeria.jpg".
+--moving JS from js/perfmatters.js into index.html, with async tag.
 --using async tag on two scripts related to Google Analytics.
 --loading fonts with script, run with async, upon document load.
---minifying index.html.
+--minifying index.html and css/print.css.
 
 
 2. For dist/views/pizza.html, to achieve a frame rate of 60 fps:
 
 Re-factored code in updatePositions().
-Moved scrolltop calculation out of for loop.
-This eliminated FSL upon scrolling the page and loading the page.
-See line 526.  Look for comment "START OPTIMIZATION".
+Moved scrolltop calculation out of for loop to eliminate FSL upon scrolling.
+Replaced querySelectorAll with getElementsByClassName, as it is faster.
+Replaced "phase" variable with an array that holds all 5 possible values.
+See line 526.  Look for comment "START THIRD OPTIMIZATION".
 
-Also: in dist/views/css/style.css:
+Changed document.addEventListener function to only
+make 40 pizzas upon document load.
+See line 572.  Look for "OPTIMIZATION FOUR".
+
+In dist/views/css/style.css:
 Added css rule "will-change: left" to .mover class.
-This promoted the mover elements to be layers, which improved performance.
+This promoted the .mover elements to be layers, which improved performance.
+No bad side effects in mobile emulation mode.
 
-Also: in dist/views/images:
+In dist/views/images:
 Resized and compressed pizzeria.jpg.
 
 
@@ -51,13 +58,19 @@ Resized and compressed pizzeria.jpg.
 
 Re-factored code in and around changePizzaSizes(size). 
 Moved determination of width outside of for loop.
-See line 462.  Look for comment "START OPTIMIZATION".
+Replaced querySelectorAll with getElementsByClassName, as it is faster.
+See line 463.  Look for comment "START SECOND OPTIMIZATION".
+
+Re-factored code of determineDx.
+Removed parameter "elem"; added parameter "oldwidth".
+Calculates oldwidth only once, rather than twice.
+See line 427.  Look for comment "START FIRST OPTIMIZATION".
 
 
 OTHER IMPROVEMENTS:
 
 Using build tools from gulp to minify JS, CSS, and HTML
-and place them in the dist folder.
+from src folder and automatically place them in the dist folder.
 
 All individual tasks run by one task named "build".
 See gulpfile.js.
--- src/views/js/main.js
+++ src/views/js/main.js
@@ -421,9 +421,15 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-  // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldwidth = elem.offsetWidth;
+  // Returns the size difference to change a pizza element from one size to another.
+  // Called by changePizzaSlices(size).
+
+  // FIRST OPTIMIZATION
+  // changed first parameter from elem to oldwidth.
+  // oldwidth can be calculated before running the determineDx function.
+
+  function determineDx (oldwidth, size) {
+    // ORIGINAL CODE: var oldwidth = elem.offsetWidth;
     var windowwidth = document.querySelector("#randomPizzas").offsetWidth;
     var oldsize = oldwidth / windowwidth;
 
@@ -447,8 +453,8 @@ var resizePizzas = function(size) {
     return dx;
   }
 
-  /* START ORIGINAL CODE:
-
+  /* START ORIGINAL CODE: */
+  /*
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
@@ -457,18 +463,23 @@ var resizePizzas = function(size) {
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
-  */ END ORIGINAL CODE
+  */
+  /* END ORIGINAL CODE */
 
-  /* START OPTIMIZATION */
-
+  /* START SECOND OPTIMIZATION */
   // Get all 100 random pizza containers.
   // They will be looped through shortly.
-  var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+
+  // Replace querySelectorAll with getElementsByClassName, as it is faster.
+  // ORIGINAL: var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+  var randomPizzas = document.getElementsByClassName("randomPizzaContainer");
 
   // Determine new width using 1 element from randomPizzas.
   // They all have the same width, so one sample is enough.
-  var dx = determineDx(randomPizzas[1], size);
-  var newwidth = (randomPizzas[1].offsetWidth + dx) + 'px';
+  // Pass determineDx the oldwidth instead of entire element.
+  var oldwidth = randomPizzas[1].offsetWidth;
+  var dx = determineDx(oldwidth, size);
+  var newwidth = (oldwidth + dx) + 'px';
 
   // Now loop through all the randomPizzas and reset their width.
   function changePizzaSizes(size) {
@@ -476,7 +487,7 @@ var resizePizzas = function(size) {
       randomPizzas[i].style.width = newwidth;
     }
   }
-  /* END OPTIMIZATION */
+  /* END SECOND OPTIMIZATION */
 
   changePizzaSizes(size);
 
@@ -523,20 +534,26 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  /* START OPTIMIZATION */
-
+  /* START THIRD OPTIMIZATION */
   // Moved scrolltop calculation out of for loop.
   // This eliminated FSL upon scrolling the page and loading the page.
+  // Also replaced querySelectorAll with getElementsByClassName, as it is faster.
+  // Also replace "phase" variable with an array that holds all 5 possible values.
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
   var scrollTop = document.body.scrollTop / 1250;
 
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(scrollTop + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  // New phase array to hold all 5 possible values.
+  var phase = [];
+  for (var j = 0; j < 5; j++) {
+    phase.push(Math.sin(scrollTop + (j % 5)));
   }
 
-  /* END OPTIMIZATION */
+  for (var i = 0; i < items.length; i++) {
+    // ORIGINAL: var phase = Math.sin(scrollTop + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase[i] + 'px';
+  }
+  /* END THIRD OPTIMIZATION */
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
@@ -552,10 +569,12 @@ function updatePositions() {
 window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
+// OPTIMIZATION FOUR: only create 40 pizzas upon load, not 200.
+
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 40; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
